5ee5958150aee962cd2c311e446594105b6a9759
Islahuddin41420110058 <84698841+Islahuddin41420110058@users.noreply.github.com>
2021-12-22 13:32:47 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -7,7 +7,7 @@ const cls_model = require('./sdk/cls_model.js');
 
 // Bot Setting
 const TelegramBot = require('node-telegram-bot-api');
-const token = '5091398105:AAHnFFJkaim8hYlIF9S_FlG0OWfwA702A-0'
+const token = '5057431556:AAGnOFgp9MlogJhT3sjbMafwdIAdpPX2L8M'
 const bot = new TelegramBot(token, {polling: true});
 
 state = 0;
@@ -26,7 +26,7 @@ bot.onText(/\/start/, (msg) => {
 bot.onText(/\/predict/, (msg) => { 
     bot.sendMessage(
         msg.chat.id,
-        `input nilai S|K contohnya 30|50`
+        `input nilai S|K|A contohnya 30|10|5`
     );   
     state = 1;
 });
@@ -37,19 +37,25 @@ bot.on('message', (msg) => {
         model.predict(
             [
                 parseFloat(s[0]), // string to float
-                parseFloat(s[1])
+                parseFloat(s[1]),
+                parseFloat(s[2])
+                
             ]
         ).then((jres1)=>{
             console.log(jres1);
             
-            cls_model.classify([parseFloat(s[0]), parseFloat(s[1]), parseFloat(jres1[0]), parseFloat(jres1[1])]).then((jres2)=>{
+            cls_model.classify([parseFloat(s[0]), parseFloat(s[1]), parseFloat(s[2]), parseFloat(jres1[0]), parseFloat(jres1[1]), parseFloat(jres1[2]) ]).then((jres2)=>{
                 bot.sendMessage(
                         msg.chat.id,
-                        `nilai pompa yang diprediksi adalah ${jres1[0]}`
+                        `nilai kipas yang diprediksi adalah ${jres1[0]}`
                 );
                 bot.sendMessage(
                         msg.chat.id,
-                        `nilai kipas yang diprediksi adalah ${jres1[1]}`
+                        `nilai pompa yang diprediksi adalah ${jres1[1]}`
+                ); 
+                bot.sendMessage(
+                        msg.chat.id,
+                        `nilai air yang diprediksi adalah ${jres1[2]}`
                 ); 
                 bot.sendMessage(
                         msg.chat.id,
@@ -67,11 +73,12 @@ bot.on('message', (msg) => {
     }
 })
 // routers
-r.get('/predict/:S/:K', function(req, res, next) {    
+r.get('/predict/:S/:K/:A', function(req, res, next) {    
     model.predict(
         [
             parseFloat(req.params.S), // string to float
-            parseFloat(req.params.K)
+            parseFloat(req.params.K),
+            parseFloat(req.params.A)
         ]
     ).then((jres)=>{
         res.json(jres);
@@ -79,38 +86,46 @@ r.get('/predict/:S/:K', function(req, res, next) {
 });
 
 //routers
-r.get('/classify/:S/:K', function(req, res, next) {    
+r.get('/classify/:S/:K/:A', function(req, res, next) {    
     model.predict(
         [
             parseFloat(req.params.S), // string to float
-            parseFloat(req.params.K)
+            parseFloat(req.params.K),
+            parseFloat(req.params.A)
         ]
     ).then((jres)=>{
         cls_model.classify(
             [
                 parseFloat(req.params.S), // string to float
                 parseFloat(req.params.K),
+                parseFloat(req.params.A),
                 parseFloat(jres[0]),
-                parseFloat(jres[1])
+                parseFloat(jres[1]),
+                parseFloat(jres[2])
             ]
         ).then((jres_)=>{
-            let status = "POMPA OFF KIPAS ON";
+            let status = "KIPAS OFF POMPA OFF AIR ON";
             
-            if(jres_ == "1|1"){
-                status = "POMPA ON KIPAS ON"
-            }if(jres_ == "1|0"){
-                status = "POMPA ON KIPAS OFF"
-            }if(jres_ == "0|0"){
-                status = "POMPA OFF KIPAS OFF"
+            if(jres_ == "1|0|1"){
+                status = "KIPAS OFF POMPA OFF AIR ON"
+            }if(jres_ == "0|0|1"){
+                status = "KIPAS ON POMPA OFF AIR OFF"
+            }if(jres_ == "0|1|1"){
+                status = "KIPAS OFF POMPA ON AIR ON"
+            }if(jres_ == "1|1|1"){
+                status = "KIPAS ON POMPA ON AIR ON"
+            }if(jres_ == "1|1|0"){
+                status = "KIPAS ON POMPA ON AIR OFF"
             }
             
 //             jres_.split("|");
             const suhu = parseFloat(req.params.S);
             const kelembaban = parseFloat(req.params.K)
+            const air = parseFloat(req.params.A)
            
             bot.sendMessage(
                     2128268907, //msg.id
-                    `SUHU:: ${suhu} KELEMBABAN ${kelembaban} KONDISI:: ${status}`
+                    `SUHU:: ${suhu} KELEMBABAN:: ${kelembaban} AIR:: ${air} KONDISI:: ${status}`
                      
                      
             ); // to telegram

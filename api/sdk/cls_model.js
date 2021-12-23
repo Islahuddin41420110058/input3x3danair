2675f0bd1315484983f6bd70fc6d27fcb417f37f
Islahuddin41420110058 <84698841+Islahuddin41420110058@users.noreply.github.com>
2021-12-23 11:03:07 +0700

    Update cls_model.js

--- api/sdk/cls_model.js
+++ api/sdk/cls_model.js
@@ -38,7 +38,7 @@ async function classify(data){
 
     try{
         // path load in public access => github
-        const path = 'https://raw.githubusercontent.com/Islahuddin41420110058/SKRIPSI/main/public/cls_model/model.json';
+        const path = 'https://raw.githubusercontent.com/Islahuddin41420110058/input3x3danair/main/public/cls_model/model.json';
         const model = await tf.loadGraphModel(path);
         
         predict = model.predict(

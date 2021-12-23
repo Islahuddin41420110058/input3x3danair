a180988d1928d19eaba8acbab6eb19522d417f54
Islahuddin41420110058 <84698841+Islahuddin41420110058@users.noreply.github.com>
2021-12-23 11:01:12 +0700

    Update cls_model.js

--- api/sdk/cls_model.js
+++ api/sdk/cls_model.js
@@ -1,30 +1,30 @@
 const tf = require('@tensorflow/tfjs-node');
 
 function normalized(data){ // i & r
-    S = (data[0] - 29.5) / 4.611213458
-    K = (data[1] - 50.5) / 28.87509493
-    O = (data[2] - 0.4) / 0.490051113
-    L = (data[3] - 0.5625) / 0.496233468
-    return [S, K, O, L]
+    N = (data[0] - 29.5) / 4.617796207
+    B = (data[1] - 9.5) / 5.197158162
+    M = (data[2] - 0.5625) / 0.496941867336809
+    A = (data[3] - 0.722222222222222) / 0.448682848877996
+    return [N, B, M, A]
 }
 
 const argFact = (compareFn) => (array) => array.map((el, idx) => [el, idx]). reduce(compareFn)[1]
 const argMax = argFact((min, el) => (el[0] > min[0] ? el : min ))
 
 function ArgMax(res){
-    label = "0|1" //POMPA OFF KIPAS ON
+    label = "0|1" //MIST MAKER OFF AIR SEDANG DIISI
     cls_data = []
     for(i=0; i<res.length; i++){
         cls_data[i] = res[i]
     }
     console.log(cls_data, argMax(cls_data));
     
-    if(argMax(cls_data) == 2){
-        label = "1|0" //POMPA ON KIPAS OFF 
-    }if(argMax(cls_data) == 0){
-        label = "0|0" //POMPA OFF KIPAS OFF 
+    if(argMax(cls_data) == 1){
+        label = "1|1" //MIST MAKER ON AIR SEDANG DIISI
+    }if(argMax(cls_data) == 2){
+        label = "0|0" //MIST MAKER OFF AIR OFF
     }if(argMax(cls_data) == 3){
-        label = "1|1" //POMPA ON KIPAS ON
+        label = "1|0" //MIST MAKER ON AIR OFF
     }
     return label
 }

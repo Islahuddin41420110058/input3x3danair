28aa60828667cc99abbbdaab321b79e1a0f384f8
Islahuddin41420110058 <84698841+Islahuddin41420110058@users.noreply.github.com>
2021-12-27 14:07:47 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -106,22 +106,22 @@ r.get('/classify/:S/:K/:A', function(req, res, next) {
                 parseFloat(jres[2])
             ]
         ).then((jres_)=>{
-            let status = "POMPA OFF KRAN ON KIPAS OFF";
+            let status = "KIPAS OFF POMPA OFF KRAN ON";
 
-            if(jres_ == "0|1|1"){
-               status = "POMPA OFF KRAN ON KIPAS ON"
-            }if(jres_ == "1|1|0"){
-               status = "POMPA ON KRAN ON KIPAS OFF"
+            if(jres_ == "1|0|1"){
+               status = "KIPAS OFF POMPA OFF KRAN ON"
+            }if(jres_ == "0|1|1"){
+               status = "KIPAS OFF POMPA ON KRAN ON"
             }if(jres_ == "1|1|1"){
-                 status = "POMPA ON KRAN ON KIPAS ON"
+                 status = "KIPAS ON POMPA ON KRAN ON"
             }if(jres_ == "0|0|0"){
-                status = "POMPA OFF KRAN OFF KIPAS OFF"
-            }if(jres_ == "0|0|1"){
-                 status = "POMPA OFF KRAN OFF KIPAS ON"
+                status = "KIPAS OFF POMPA OFF KRAN OFF"
             }if(jres_ == "1|0|0"){
-                status = "POMPA ON KRAN OFF KIPAS OFF"
-            }if(jres_ == "1|0|1"){
-                status = "POMPA ON KRAN OFF KIPAS ON"
+                 status = "KIPAS ON POMPA OFF KRAN OFF"
+            }if(jres_ == "0|1|0"){
+                status = "KIPAS OFF POMPA ON KRAN OFF"
+            }if(jres_ == "1|1|0"){
+                status = "KIPAS ON POMPA ON KRAN OFF"
              }
             
 //             jres_.split("|");
